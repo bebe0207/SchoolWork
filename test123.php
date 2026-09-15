9d3be4b6b76521b2a8b6d9eea81c55ad255f9d31
bebe0207 <opennov13@gmail.com>
2026-09-15 15:28:01 +0800

    123456

--- test123.php
+++ test123.php
@@ -1,1 +1,11 @@
-<?php echo "hello world"; ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>網頁標題</title>
+</head>
+<body>
+    <h1>這是主標題</h1>
+    <p>這是段落文字。</p>
+</body>
+</html>
