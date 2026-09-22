d29e3e95918a586f6f61ef53cab59821fa54b142
bebe0207 <opennov13@gmail.com>
2026-09-22 15:54:02 +0800

    Fix BMI input validation and conditional syntax

--- test copy.js
+++ test copy.js
@@ -1,25 +1,23 @@
-var weight = 60; var height = 160; 
 var readline = require("readline-sync");
-var weight = readline.question("your weight?(kg)1-200:");
-var height = readline.question("your height(cm)1-250:");
-if (weight <= 0 || height <= 0) { 
-    console.log("Please enter a valid weight and height."); 
-}
-else{
-    var bmi = weight / ((height / 100) ** 2); 
-    console.log("Your BMI:" + bmi);  
-}
-if (weight > 200 && height > 250) ;{
+
+var weight = Number(readline.question("your weight?(kg)1-200:"));
+var height = Number(readline.question("your height(cm)1-250:"));
+
+if (
+    !Number.isFinite(weight) ||
+    !Number.isFinite(height) ||
+    weight < 1 || weight > 200 ||
+    height < 1 || height > 250
+) {
     console.log("Please enter a valid weight and height.");
-}
-else{
-    var bmi = weight / ((height / 100) ** 2); 
-    console.log("Your BMI:" + bmi);  
+} else {
+    var bmi = weight / ((height / 100) ** 2);
+    console.log("Your BMI: " + bmi.toFixed(2));
 }
 
-//function test(weight=70){     
-//    var weight=50;     
-//    console.log("weight="+weight)     
-//    return weight; }  
-//var result=test(90);  
+//function test(weight=70){
+//    var weight=50;
+//    console.log("weight="+weight)
+//    return weight; }
+//var result=test(90);
 //console.log("weight="+weight)
