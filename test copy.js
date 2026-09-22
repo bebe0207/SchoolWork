c80d86ffe4a05868980c49fb3d4f201991e6931b
bebe0207 <opennov13@gmail.com>
2026-09-22 15:58:37 +0800

    s1

--- test copy.js
+++ test copy.js
@@ -1,9 +1,22 @@
-//var weight = 60; var height = 160; 
+var weight = 60; var height = 160; 
 var readline = require("readline-sync");
-var weight = readline.question("請輸入體重(kg):");
-var height = readline.question("請輸入身高(cm):");
-var bmi = weight / (height / 100 ** 2); 
-console.log("Your BMI:" + bmi);  
+var weight = readline.question("your weight?(kg)1-200:");
+var height = readline.question("your height(cm)1-250:");
+if (weight <= 0 || height <= 0) { 
+    console.log("Please enter a valid weight and height."); 
+}
+else{
+    var bmi = weight / ((height / 100) ** 2); 
+    console.log("Your BMI:" + bmi);  
+}
+if (weight > 200 && height > 250) ;{
+    console.log("Please enter a valid weight and height.");
+}
+else{
+    var bmi = weight / ((height / 100) ** 2); 
+    console.log("Your BMI:" + bmi);  
+}
+
 //function test(weight=70){     
 //    var weight=50;     
 //    console.log("weight="+weight)     
