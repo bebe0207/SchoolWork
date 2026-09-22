262b6a9fbb6c1ed2d3534892339c37ca727c2b7a
bebe0207 <opennov13@gmail.com>
2026-09-22 15:42:38 +0800

    456

--- test copy.js
+++ test copy.js
@@ -1,9 +1,12 @@
-var weight = 60; var height = 160; 
+//var weight = 60; var height = 160; 
+var readline = require("readline-sync");
+var weight = readline.question("請輸入體重(kg):");
+var height = readline.question("請輸入身高(cm):");
 var bmi = weight / (height / 100 ** 2); 
 console.log("Your BMI:" + bmi);  
-function test(weight=70){     
-    var weight=50;     
-    console.log("weight="+weight)     
-    return weight; }  
-var result=test(90);  
-console.log("weight="+weight)
+//function test(weight=70){     
+//    var weight=50;     
+//    console.log("weight="+weight)     
+//    return weight; }  
+//var result=test(90);  
+//console.log("weight="+weight)
